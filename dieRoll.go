d66e0e8e54d26dcc91dc339c17ed362dbee458a7
neville <nmascarenhas@arubanetworks.com>
2019-03-15 15:52:12 -0700

    multiparams

--- dieRoll.go
+++ dieRoll.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"strconv"
 	"time"
 )
 
@@ -15,8 +16,17 @@ func twodieRoll(size1, size2 int) (int, int) {
 	return onedieRoll(size1), onedieRoll(size2)
 }
 
+func returnsNamed(ip1 string, ip2 int) (theresult string, err error) {
+	theresult = "modified " + ip1 + " , " + strconv.Itoa(ip2)
+	return theresult, err
+}
+
 func main() {
 	fmt.Println("Hi")
 	fmt.Println(onedieRoll(20))
 	fmt.Println(twodieRoll(25, 35))
+
+	named, err := returnsNamed("globule", 42)
+	fmt.Printf("Named params returned: '%s', %v\n", named, err)
+
 }
