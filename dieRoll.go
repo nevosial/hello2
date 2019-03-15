77757fff4145802bcd830b877f1dea0e11c6f2d6
neville <nmascarenhas@arubanetworks.com>
2019-03-15 15:36:11 -0700

    dirolls

--- dieRoll.go
+++ dieRoll.go
@@ -6,12 +6,17 @@ import (
 	"time"
 )
 
-func dieRoll(size int) int {
+func onedieRoll(size int) int {
 	rand.Seed(time.Now().UnixNano())
-	return rand.Intn(6) + 1
+	return rand.Intn(size) + 1
+}
+
+func twodieRoll(size1, size2 int) (int, int) {
+	return onedieRoll(size1), onedieRoll(size2)
 }
 
 func main() {
 	fmt.Println("Hi")
-	fmt.Println(dieRoll(6))
+	fmt.Println(onedieRoll(20))
+	fmt.Println(twodieRoll(25, 35))
 }
