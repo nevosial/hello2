fdc0867b8e71f8484901f9bbf20418e37fd1165a
nevosial <nevosial@gmail.com>
2019-03-17 12:37:34 -0700

    controls in golang

--- gocontrols.go
+++ gocontrols.go
@@ -1,32 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
 
-func printeven(){
-	for i:=0; i<=10; i++{
-		if i%2==0 {
+func printeven() {
+	for i := 0; i <= 10; i++ {
+		if i%2 == 0 {
 			fmt.Println(i)
 		}
 	}
 }
 
 func getprice(a string) int {
-	switch a{
-		case "apple":
-			return 10
-		case "bun":
-			return 50
-		default: 
-			return 0
+	switch a {
+	case "apple":
+		return 10
+	case "bun":
+		return 50
+	default:
+		return 0
 	}
 }
 
+func findOS() string {
+	os := runtime.GOOS
+	switch os {
+	case "darwin":
+		return "OS X"
+	case "linux":
+		return "Linux"
+	default:
+		return os
+	}
+}
 
-func main(){
+//switch without condition
+func swoc() {
+	t := time.Now()
+
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Good morning !")
+	case t.Hour() < 17:
+		fmt.Println("Good Afternoon !")
+	default:
+		fmt.Println("Good Evening !")
+	}
+}
+
+func main() {
 	printeven()
 	println(getprice("apple"))
 	println(getprice("bun"))
 	println(getprice("kela"))
-
+	print("Go runs on: ")
+	println(findOS())
+	swoc()
 }
-
